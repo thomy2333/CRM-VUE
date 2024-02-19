4bfb9a655efd1a7b949e7c86e0ece85ff0cd96af
thomy2333 <thomyrb30@gmail.com>
2024-02-19 11:06:23 -0300

    first commit

---
+++ src/components/UI/RouterLink.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+
+    defineProps({
+        to: {
+            type: String
+        }
+    })
+</script>
+
+<template>
+    <RouterLink 
+        :to="{ name: to }"
+        class="block md:inline-block rounded bg-blue-800 py-2 px-3 text-white uppercase font-bold text-sm shadow hover:bg-blue-600"
+    >
+        <slot></slot>
+    </RouterLink>
+</template>
